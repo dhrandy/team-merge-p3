6ad20b4bab516170d38c85fe18d496f5fe497e46
dhrandy <dhrandy@gmail.com>
2018-08-07 21:00:58 -0400

    update

---
+++ client/src/components/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './homePage.css';
+
+export default class HomePage extends Component {
+    render() {
+      return (
+        <p>home page</p>
+      );
+    };
+};
