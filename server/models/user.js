24ff22f7fe0e5d4b98320d07b9de74d50e5fda01
Matthew Curran <five3183@gmail.com>
2018-08-07 20:20:26 -0400

    update

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose")
-const bcrypt = require("bcryptjs")
-
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -11,19 +9,10 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
-    username: {
-        type: String,
-        required: true
-    },
     password: {
         type: String,
         required: true
-    },
-    prescriptions: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Prescription'
-    }]
-
+    }
 })
 
 const User = module.exports = mongoose.model("User", UserSchema)
@@ -35,21 +24,4 @@ module.exports.getUserById = (id, callback) => {
 module.exports.getUserByUsername = (username, callback) => {
     const query = {username: username}
     User.findOne(query, callback)
-}
-
-module.exports.addUser = (newUser, callback) => {
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if(err) throw err
-            newUser.password = hash
-            newUser.save(callback)
-        })
-    })
-}
-
-module.exports.comparePassword = (password, hash, callback) => {
-    bcrypt.compare(password, hash, (err, isMatch) => {
-        if(err) throw err
-        callback(null, isMatch)
-    })
 }
