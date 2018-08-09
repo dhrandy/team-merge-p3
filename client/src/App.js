0500607f717592e4980474ccc35053efde67d569
dhrandy <dhrandy@gmail.com>
2018-08-09 19:37:30 -0400

    added home

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Wrapper from "./components/Wrapper/Wrapper"
-import Login from "./components/Login/Login"
-import Register from "./components/Register/Register"
-import Header from "./components/Header/Header"
-import BottomNavbar from "./components/Navbar/Bottom/BottomNavbar"
-import TopNavbar from "./components/Navbar/Top/TopNavbar"
+import Wrapper from "./components/Wrapper/Wrapper";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Header from "./components/Header/Header";
+import BottomNavbar from "./components/Navbar/Bottom/BottomNavbar";
+import TopNavbar from "./components/Navbar/Top/TopNavbar";
+import HomePage from "./components/HomePage/Home";
 
 const App = () => (
   <Router>
@@ -13,6 +14,7 @@ const App = () => (
       <TopNavbar />
       <Header />
       <Wrapper>
+        <Route exact path="/" component={HomePage} />
         {/* <Route exact path="/" component={About} /> */}
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,13 +3,19 @@ import './header.css'
 
 export default class Header extends Component {
   render() {
+    function tick() {
+      let time = (Date().toLocaleTimeString());
+      time.setInterval(tick, 1000);
+    }
     return (
       <React.Fragment>
         <div className='jumbotron'>
-          <div className= "time">{(new Date()).toLocaleString()}
+          <div className= "time">{tick}
           </div>
         </div>
       </React.Fragment>
     );
   };
 };
+
+// (new Date()).toLocaleString()
