942b292ad73c042f0c011ae0c4773f11cb78657e
Karina Solorio <macuser@wifi_guest-10-38-124-178.uncc.edu>
2018-08-14 19:59:41 -0400

    fixed counter component

--- client/src/components/Counter/Counter.js
+++ client/src/components/Counter/Counter.js
@@ -3,34 +3,58 @@ import './counter.css'
 
 
 class Counter extends Component {
-  state= {count:0}
+  constructor(props) {
+    super(props);
+    this.state= {
+      clicks: 0,
+      show:true
+    };
+  }
 
-  increment = e => this.setState ({ 
-    count: this.state.count +1
-  })
+  increment = () => { 
+    this.setState({ clicks: this.state.clicks +1});
+  }
 
-  decrement = e =>
-  this.setState({ count: this.state.count -1})
+  decrement = () => { 
+    this.setState({clicks: this.state.clicks -1});
+  }  
+
+  toggleClick = () => {
+    this.setState({ show: !this.state.show});
+  }
   
 
-  render = () =>
-  <div>
-    <div class="input-group">
-      <span class="input-group-btn">
-        <button onClick={this.increment} type="button" class="btn btn-secondary btn-number" data-type="minus" data-field="">
-        +
-        </button>
-      </span>
+  render () {
+    return (
+  <div className="container">
+    <div className="row input-group">
 
-      <input type="text" id="quantity" name="quantity" size="2" value="1" min="1" max="100" />
+      <div className='col-md'>
+        <span className="input-group-btn">
+          <button onClick={this.increment} type="button" className="btn btn-secondary btn-number" data-type="minus" data-field="">
+          +
+          </button>
+        </span>
+      </div>
 
-      <span class="input-group-btn">
-        <button onClick={this.decrement} type="button" class= "btn btn-secondary btn-number" data-type="plus" data-field="">
+      <div className='col-md'>
+          { this.state.show ? <span id="number">{ this.state.clicks }</span> : '' }
+      {/* <input type="text" id="quantity" name="quantity" size="2" value="1" min="1" max="100"  /> */}
+      </div>
+      
+      <div className='col-md'>
+      <span className="input-group-btn">
+        <button onClick={this.decrement} type="button" className= "btn btn-secondary btn-number" data-type="plus" data-field="">
         -
         </button>
       </span>
+      </div>
+
     </div>
   </div>
+  )
+  }
+
 }
 
 export default Counter
