9f7b9a4854732f34118edd7bd60eae2422ba32b0
dhrandy <dhrandy@gmail.com>
2018-08-20 12:43:22 -0400

    changed href to Link

--- client/src/components/Account/AccountContainer.js
+++ client/src/components/Account/AccountContainer.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import axios from 'axios';
-import Account from './Account.js';
-import drugs from '../../prescription.json'
+import Account from './Account';
+//import drugs from '../../prescription.json'
 import Wrapper from '../Wrapper/Wrapper'
-import Account from '../../components/Account/Account'
 
-export default class AccountContainer extends React.Component {
-    state = {
-        drugs
-    }
+const AccountContainer = (props) => {
+    const userData = props.userData
+    console.log('DEBUG - AccountContainer - userData', userData)
+    return (
+        <div className='container'>
 
-    render() {
-        console.log(props)
-        return (
-            <React.Fragment>
-                {this.state.drugs.map((drug, index) =>
-                    <div key={index}>
-                        <Account
-                            prescription={drug.drugs}
-                            Medication_Conflict={drug.Medication_Conflict}
-                            Food_Restrictions={drug.Food_Restrictions}
-                            Medicated={drug.Medicated}
-                        />
-                    </div>
-                )}
-            </React.Fragment>
-        )
-    }
+            <div className='row'>
+                <div className='col-md'>
+                    <h2> Dont forget to take </h2>
+                </div>
+            </div>
+
+            <div className='row'>
+                <div className='col-md'>
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Prescription</th>
+                                <th scope="col">Medication Conflict</th>
+                                <th scope="col">Food Restrictions</th>
+                                <th scope="col">Activity Restrictions</th>
+                                <th scope="col">Medicated </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {userData.prescriptions.map((drug, index) =>
+                                <Account  key={index}
+                                    prescription={drug.name}
+                                    medicationConflict={drug.medicationRestrictions.length}
+                                    foodRestrictions={drug.foodRestrictions.length}
+                                    activityRestrictions={drug.activityRestrictions.length}
+                                    Medicated={userData.name}
+                                />
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
 }
+
+export default AccountContainer
