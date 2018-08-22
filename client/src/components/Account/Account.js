34aa8cd5fe5d25daa7dba74dd34c196df71e8954
Solorio <solorio_michelle@yahoo.com>
2018-08-21 21:35:33 -0400

    adding icons

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -4,11 +4,12 @@ import './Account.css'
 const Account = props => (
     <tr>
         <td>{props.prescription}</td>
-        <td>{props.medicationConflict}</td>
-        <td>{props.foodRestrictions}</td>
-        <td>{props.activityRestrictions}</td>
+        <td>{props.medicationConflict}<i className="fas fa-tablets fa-md "></i></td>
+        <td>{props.foodRestrictions}<i className="fas fa-apple-alt fa-2x"></i></td>
+        <td>{props.activityRestrictions}<i className="fas fa-people-carry fa-2x "></i></td>
         <td>{props.Medicated}</td>
     </tr>
+
 );
 
 export default Account
